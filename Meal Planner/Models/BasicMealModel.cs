72ad92945e4b93f3139dd095e760dd84b981353a
Kyle A <kyleandrews@mail.fresnostate.edu>
2021-02-07 04:32:39 +0000

    Revert "Revert "Initial Project Commit""
    This reverts commit e739270448488becc0c459aa98156db873f38db6.

---
+++ Meal Planner/Models/BasicMealModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Meal_Planner.Models
+{
+    public class BasicMealModel
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+        public string instructions { get; set; }
+
+        //public List<ingredients> {get;set;}
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Nutrition-Tracker 
 # Website using ASP.NET for CSCI 152
+# When the project is out of sync and it contains newly created/syned Models remember to run Update-Database in the Package Manager Console
