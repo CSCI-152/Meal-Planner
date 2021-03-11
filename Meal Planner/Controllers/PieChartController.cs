a70987790ddeba0ab6c12f6efa2684bb3c08ad58
EvanaHolevas <70241021+EvanaHolevas@users.noreply.github.com>
2021-03-11 02:17:23 -0800

    Added chart model and controller, modified dashboard Html
    I'm having a hard time aligning the chart on the page. It keeps clipping through other content.
     PieChartController currently has constants for testing.

---
+++ Meal Planner/Controllers/PieChartController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Meal_Planner.Models;
+
+namespace Meal_Planner.Models
+{
+    public class PieChartController : Controller
+    {
+		public ActionResult Index()
+		{
+			List<ChartDataPoint> dataPoints = new List<ChartDataPoint>();
+
+			dataPoints.Add(new ChartDataPoint("Carbohydrates", 25));    // controller will need to get actual values to fill in for constants
+			dataPoints.Add(new ChartDataPoint("Fats", 13));
+			dataPoints.Add(new ChartDataPoint("Protein", 8));
+
+			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+
+			return View();
+		}
+	}
+}
