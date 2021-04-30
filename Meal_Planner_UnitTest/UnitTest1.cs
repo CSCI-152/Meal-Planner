e04db661af8b534aa8ae3f3551846357263ad452
EvanaHolevas <70241021+EvanaHolevas@users.noreply.github.com>
2021-04-29 17:58:11 -0700

    Created testing project

---
+++ Meal_Planner_UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Meal_Planner;
+
+namespace Meal_Planner_UnitTest
+{
+    public class Tests
+    {
+        private const string Expected = "Hello World!";
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
